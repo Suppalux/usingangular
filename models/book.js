4cb1708a446963bd5929c318ab8545c7b0e333b1
Suppalux <maximumretro80@gmail.com>
2020-03-11 01:11:52 +0700

    add comment

--- models/book.js
+++ models/book.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var booksSchema = new Schema({
-isbn: String,
-title: String,
-price: Number
+isbn: String, //Code Book in mongo
+title: String, //Name Book in mongo
+price: Number //Price Book in mongo
 });
 
 module.exports =mongoose.model('books', booksSchema);
